54c73d5e4cfccb6c8386c6e2ab2587f076a0a49d
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-11-11 14:17:01 -0800

    Added dynamic links for Movie component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+// import {Movie, MovieList} from './Movies';
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -9,13 +12,13 @@ const App = () => {
     setSavedList( [...savedList, movie] );
   };
 
-  <Route exact path='/' component={MovieList} />
-  <Route path='/movies/:id' component={Movie} />
-
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Route exact path='/' component={MovieList} />
+        <Route path='/movies/:id' component={Movie} />
+      </div>
     </div>
   );
 };
